9feb6d45de450c1294b1a5bdb07d8af477e0e754
Mohammed AbuGrara <abugrara@gmail.com>
2018-01-27 17:54:29 +0200

    'remove Home route'

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index');
 Route::resource('users','UserController');
 Route::resource('cars','CarController');
 Route::resource('reviews','ReviewController');
