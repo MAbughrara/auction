dd0ba4d034e5a6b3c7b0bf6343522d6f9547289e
Socotoly <socotoly@live.com>
2018-01-29 18:06:40 +0200

    footer tweaks

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,6 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-3">
-                <h2 class="logo"><a href="#"> LOGO </a></h2>
             </div>
             <div class="col-sm-2">
                 <h5>Get started</h5>
@@ -21,14 +20,6 @@
                     <li><a href="#" data-toggle="modal" data-target="#contact">Contact us</a></li>
                 </ul>
             </div>
-            {{--<div class="col-sm-2">--}}
-            {{--<h5>Support</h5>--}}
-            {{--<ul>--}}
-            {{--<li><a href="#">FAQ</a></li>--}}
-            {{--<li><a href="#">Help desk</a></li>--}}
-            {{--<li><a href="#">Forums</a></li>--}}
-            {{--</ul>--}}
-            {{--</div>--}}
             <div class="col-sm-3 col-sm-offset-2">
                 <div class="social-networks">
                     <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
@@ -45,6 +36,3 @@
     </div>
 </footer>
 @include('layouts.contact')
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
