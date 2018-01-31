427bd91c13719d4ea0ddb17e98580d1043f618e3
socotoly <socotoly@live.com>
2018-02-01 00:04:57 +0200

    deleted unnecessary files

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -93,23 +93,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 <script src="{{ asset('js/admin/charts-home.js') }}"></script>
 <script src="{{ asset('js/admin/front.js') }}"></script>
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID.-->
-<!---->
-<script>
-    (function (b, o, i, l, e, r) {
-        b.GoogleAnalyticsObject = l;
-        b[l] || (b[l] =
-            function () {
-                (b[l].q = b[l].q || []).push(arguments)
-            });
-        b[l].l = +new Date;
-        e = o.createElement(i);
-        r = o.getElementsByTagName(i)[0];
-        e.src = '//www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e, r)
-    }(window, document, 'script', 'ga'));
-    ga('create', 'UA-XXXXX-X');
-    ga('send', 'pageview');
-</script>
 </body>
 </html>
